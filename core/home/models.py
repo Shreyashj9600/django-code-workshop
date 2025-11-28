e1be1d79a10449a450affb7378d59b584f810724
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-11-29 01:17:45 +0530

    models and migrations

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField() 
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+    image =  models.ImageField()
+    file = models.FileField()
